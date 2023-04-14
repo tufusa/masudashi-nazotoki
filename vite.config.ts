45be9cd781fc861dfc178a9ad56e21d1e63596dc
Tufusa <louptung@gmail.com>
2023-04-14 16:46:06 +0900

    Add

--- vite.config.ts
+++ vite.config.ts
@@ -10,23 +10,6 @@ export default defineConfig({
     open: '/'
   },
   build: {
-    cssCodeSplit: false,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        play: resolve(__dirname, 'play/index.html'),
-        clear: resolve(__dirname, 'clear/index.html'),
-        credits: resolve(__dirname, 'credits/index.html'),
-      },
-      output: {
-        entryFileNames: 'assets/[name].js',
-        // assetFileNames: 'assets/[name].[ext]'
-        assetFileNames: (assetInfo) => {
-          let basename = assetInfo.name.split('.')[0]
-          let name = assetInfo.name.split('.')[1] == "css" ? "index" : basename
-          return `assets/${name}.[ext]`
-        }
-      },
-    },
-  },
+    outDir: 'docs'
+  }
 })
