8ece5cdf78ee99b9032d8c86c966161f7eb75ed8
Tufusa <louptung@gmail.com>
2023-04-14 17:45:00 +0900

    Modify

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: process.env.GITHUB_PAGES
-      ? "masudashi_nazotoki"
+      ? "masudashi-nazotoki"
       : "./",
   server: {
     open: '/'
