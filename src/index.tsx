eeb4278e131cdd757fa6d6cf5cdd89753b7251b7
tufusa <tufusa74@gmail.com>
2025-06-27 21:26:07 +0900

    chore: CU

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { RouterProvider } from 'react-router-dom';
 import router from 'routes/router';
-import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <RouterProvider router={router} />
-    </RecoilRoot>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
